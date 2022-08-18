e9e8da24c8c7f5dc2b1404e7090394e8348485f8
jlvgit <jlv44@ymail.com>
2022-08-17 20:37:37 -0400

    Update links

--- src/components/categoryLinksComponent.js
+++ src/components/categoryLinksComponent.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import { Container, Row, Col} from 'reactstrap';
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+
+import bookstore from "../images/bookstore.jpg";
+import readingspot from "../images/readingspot.jpg";
+import showbook from "../images/showbook.jpg";
 
 const CategoryLinks = () => {
-	return (
+    return (
         <Container fluid={true}>
             <Row className="row-cols-1">
                 <Col className="col-md-4 text-center mb-5 p-0">
@@ -10,7 +14,7 @@ const CategoryLinks = () => {
                         <p className="section-title">BOOKS</p>
                     </div>
                     <a href="#">
-                        <img src="imgs/bookstore.jpg" className="img-fluid w-75 zoom" alt="bookstore" />
+                        <img src={bookstore} className="img-fluid w-75 zoom" alt="bookstore" />
                     </a>
                 </Col>
                 <Col className="col-md-4 text-center mb-5 p-0">
@@ -18,7 +22,7 @@ const CategoryLinks = () => {
                         <p className="section-title">READING SPOTS</p>
                     </div>
                     <a href="#">
-                        <img src="imgs/readingspot.jpg" className="img-fluid w-75 zoom" alt="readingspot" />
+                        <img src={readingspot} className="img-fluid w-75 zoom" alt="readingspot" />
                     </a>
                 </Col>
                 <Col className="col-md-4 text-center mb-5 p-0">
@@ -26,12 +30,12 @@ const CategoryLinks = () => {
                         <p className="section-title">RECOMMENDATIONS</p>
                     </div>
                     <a href="#">
-                        <img src="imgs/showbook.jpg" className="img-fluid w-75 zoom" alt="showbook" />
+                        <img src={showbook} className="img-fluid w-75 zoom" alt="showbook" />
                     </a>
                 </Col>
             </Row>
         </Container>
-	);
+    );
 };
 
 export default CategoryLinks;
--- src/shared/featuredBooks.js
+++ src/shared/featuredBooks.js
@@ -1,26 +1,31 @@
+import sineater from "../images/sineater.jpg";
+import outsider from "../images/outsider.jpg";
+import bbcdune from "../images/bbcdune.jpg";
+import lightbeyond from "../images/lightbeyond.jpg";
+
 export const FEATURED = [
     {
         id: 0,
         title: "Sin Eater",
-        image: "/imgs/sineater.jpg",
-        blurb: "A stunning combination of Sins and Eating!"
+        image: sineater,
+        blurb: "A stunning combination of Sins and Eating!",
     },
     {
         id: 1,
-        title:"Outsider",
-        image: "imgs/outsider.jpg",
-        blurb: "Look in from the outside with Outsider!"
+        title: "Outsider",
+        image: outsider,
+        blurb: "Look in from the outside with Outsider!",
     },
     {
         id: 2,
-        title:"Dune",
-        image: "imgs/bbcdune.jpg",
-        blurb: "Herbert's classic, gets a new cover!"
+        title: "Dune",
+        image: bbcdune,
+        blurb: "Herbert's classic, gets a new cover!",
     },
     {
         id: 3,
-        title:"Light Beyond",
-        image: "imgs/lightbeyond.jpg",
-        blurb: "It's got a tree and a sun presumably!"
-    }
+        title: "Light Beyond",
+        image: lightbeyond,
+        blurb: "It's got a tree and a sun presumably!",
+    },
 ];
